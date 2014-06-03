1649950538d35bf1df3e26f9f06f419663ecb4c0
frenzyjay <jay.frenzystuff@gmail.com>
2014-06-03 18:02:55 +0200

    add sdl_image

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: npineau <npineau@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2014/03/08 10:39:32 by npineau           #+#    #+#              #
-#    Updated: 2014/06/02 18:17:19 by jvincent         ###   ########.fr        #
+#    Updated: 2014/06/03 16:07:27 by jvincent         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -43,6 +43,7 @@ HEADSRV	:=	$(SERVEUR).h
 HEADGFX	:=	gfx.h
 LIB		:=	libft.a
 SLIB	:=	libSDL2.a
+SILIB	:=	SDL2_image.a
 
 ### PATHS ###
 
@@ -61,7 +62,7 @@ PSDL		:=	$(DIRSDL)/lib/$(SLIB)
 CC		:=	llvm-gcc
 C_FLAG	:=	-Wall -Wextra -Werror
 O_FLAG	:=	-O3
-L_FLAG	:=	-L $(DIRLIB) -lft -L $(DIRSDL)/lib -lSDL2
+L_FLAG	:=	-L $(DIRLIB) -lft -L $(DIRSDL)/lib -lSDL2 -lSDL2_image
 C_INC	:=	-I $(DIRINC) -I $(DIRLIB)/$(DIRINC) -I $(DIRSDL)/include/$(DIRSDL)/
 
 COMPIL	=	$(CC) -o $@ -c $< $(C_INC) $(C_FLAG) $(O_FLAG)
@@ -83,6 +84,12 @@ $(PSDL):
 		&& ./configure CC=clang --prefix=$(shell pwd)/$(DIRSDL)/ \
 		&& $(MAKE) && $(MAKE) install )
 	mv SDL2-2.0.3.tar.gz SDL2-2.0.3 SDL2/junk
+	curl -O https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.0.tar.gz
+	tar xf SDL2_image-2.0.0.tar.gz
+	( cd SDL2_image-2.0.0 \
+		&& ./configure CC=clang OBJC=clang --prefix=$(shell pwd)/$(DIRSDL)/ \
+		&& $(MAKE) && $(MAKE) install )
+	mv SDL2_image-2.0.0.tar.gz SDL2_image-2.0.0 SDL2/junk
 
 ### LIBFT ###
 
--- sources/gfx/gfx.c
+++ sources/gfx/gfx.c
@@ -6,11 +6,12 @@
 /*   By: jvincent <jvincent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/06/02 18:17:54 by jvincent          #+#    #+#             */
-/*   Updated: 2014/06/03 14:25:41 by jvincent         ###   ########.fr       */
+/*   Updated: 2014/06/03 16:16:35 by jvincent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "SDL.h"
+#include "SDL_image.h"
 #include "gfx.h"
 
 int	init_sdl(t_sdl *e)
@@ -34,10 +35,39 @@ void	close_sdl(t_sdl *e)
 void	draw_crap(t_sdl *e)
 {
 	int	color;
+	SDL_Surface	*image;
 
 	color = SDL_MapRGB(e->screen->format, 0xDC, 0x14, 0x3C);
 	SDL_FillRect(e->screen, NULL, color);
+
+	image = IMG_Load("img/poo.gif");
+	SDL_Rect rcDest = {10, 10, 0, 0};
+	SDL_BlitSurface(image, NULL, e->screen, &rcDest);
 	SDL_UpdateWindowSurface(e->win);
+}
+
+int	quit_listener(SDL_Event *ev, int *quit)
+{
+	while (SDL_PollEvent(ev) != 0)
+	{
+		if (ev->type == SDL_QUIT)
+			*quit = 1;
+	}
+	return (0);
+}
+
+int	gfx_core(t_sdl *e)
+{
+	SDL_Event	ev;
+	int			quit;
+
+	quit = 0;
+	while (!quit)
+	{
+		quit_listener(&ev, &quit);
+		draw_crap(e);
+	}
+	return (0);
 }
 
 int	main(int argc, char **argv)
@@ -47,10 +77,9 @@ int	main(int argc, char **argv)
 	(void)argc;
 	(void)argv;
 
-	ft_printf("GFX on.");
+	ft_putendl("GFX on.");
 	init_sdl(&e);
-	draw_crap(&e);
-	SDL_Delay(2000);
+	gfx_core(&e);
 	close_sdl(&e);
 	return (0);
 }

eb5d729b0ec1906e9dde95ab4978a37525b307c3
frenzyjay <jay.frenzystuff@gmail.com>
2014-06-04 05:30:55 +0200

    add board scrolling :) next step : panels / viewports

--- includes/gfx.h
+++ includes/gfx.h
@@ -7,14 +7,42 @@
 # define FS (SDL_WINDOW_SHOWN)
 # define WIDTH 1920
 # define HEIGHT 1080
+# define MAP_W 10109
+# define MAP_H 4542
+# define SQUARE_W 50
 # define KEY_ESC 27
 # define KEY_ENTER 13
 # define KEY_SPACE 32
+# define KEY_UP 1073741906
+# define KEY_DOWN 1073741905
+# define KEY_LEFT 1073741904
+# define KEY_RIGHT 1073741903
 
-typedef struct	s_sdl
+typedef struct		s_sdl
 {
-	SDL_Window	*win;
-	SDL_Surface	*screen;
-}				t_sdl;
+	SDL_Window		*win;
+	SDL_Renderer	*screen;
+	SDL_Texture		*view;
+}					t_sdl;
+
+typedef struct s_env
+{
+	t_sdl		e;
+	int			camera[2];
+	int			scroll[4];
+	int			s_speed;
+}				t_env;
+
+typedef struct		s_case
+{
+	int				stones[7];
+	int				food;
+}					t_case;
+
+typedef struct		s_game
+{
+	t_case			map[MAP_W][MAP_H];
+	int				nb_players;
+}					t_game;
 
 #endif
--- sources/gfx/gfx.c
+++ sources/gfx/gfx.c
@@ -6,7 +6,7 @@
 /*   By: jvincent <jvincent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/06/02 18:17:54 by jvincent          #+#    #+#             */
-/*   Updated: 2014/06/03 22:59:11 by jvincent         ###   ########.fr       */
+/*   Updated: 2014/06/04 05:28:10 by jvincent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,59 +15,121 @@
 #include "SDL_image.h"
 #include "gfx.h"
 
-int	init_sdl(t_sdl *e)
+SDL_Texture	*load_texture(t_env *gfx, char *str)
 {
+	SDL_Texture	*texture;
+	SDL_Surface	*image;
+
+	texture = NULL;
+	image = IMG_Load(str);
+	texture = SDL_CreateTextureFromSurface(gfx->e.screen, image);
+	SDL_FreeSurface(image);
+	return (texture);
+}
+
+int	init_sdl(t_env *gfx)
+{
+	int	img_flag;
+
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 		return (ft_error("SDL_Init failed."));
-	e->win = SDL_CreateWindow("GFX ZAPPY BETA", SDL_WPU, SDL_WPU, WIDTH, HEIGHT, FS);
-	if (e->win == NULL)
+	gfx->e.win = SDL_CreateWindow("GFX ZAPPY", SDL_WPU, SDL_WPU, WIDTH, HEIGHT, FS);
+	if (gfx->e.win == NULL)
 		return (ft_error("Can't create window."));
-	e->screen = SDL_GetWindowSurface(e->win);
+	gfx->e.screen = SDL_CreateRenderer(gfx->e.win, -1, SDL_RENDERER_ACCELERATED);
+	SDL_SetRenderDrawColor(gfx->e.screen, 0xFF, 0xFF, 0xFF, 0xFF);
+	img_flag = IMG_INIT_PNG;
+	IMG_Init(img_flag);
+	gfx->e.view = load_texture(gfx, "img/board.jpg");
+	gfx->camera[0] = 0;
+	gfx->camera[1] = 0;
+	gfx->scroll[0] = 0;
+	gfx->scroll[1] = 0;
+	gfx->scroll[2] = 0;
+	gfx->scroll[3] = 0;
+	gfx->s_speed = 1;
 	return (0);
 }
 
-void	close_sdl(t_sdl *e)
+void	close_sdl(t_env *gfx)
 {
-	SDL_FreeSurface(e->screen);
-	SDL_DestroyWindow(e->win);
+	SDL_DestroyWindow(gfx->e.win);
 	SDL_Quit();
 }
 
-void	draw_bg(t_sdl *e)
+void	draw_menu(t_env *gfx)
 {
-	int	color;
-
-	color = SDL_MapRGB(e->screen->format, 0xDC, 0x14, 0x3C);
-	SDL_FillRect(e->screen, NULL, color);
-}
-
-void	draw_menu(t_sdl *e)
-{
-	int			color;
 	SDL_Rect	rcDest;
 
 	rcDest.x = 1620;
 	rcDest.y = 0;
 	rcDest.w = 300;
 	rcDest.h = 1080;
-	color = SDL_MapRGB(e->screen->format, 0xCE, 0xCE, 0xCE);
-	SDL_FillRect(e->screen, &rcDest, color);
+	SDL_SetRenderDrawColor(gfx->e.screen, 0x0E, 0x28, 0x49, 0xFF);
+	SDL_RenderDrawRect(gfx->e.screen, &rcDest);
 }
 
-void	draw_crap(t_sdl *e)
+void	draw_board(t_env *gfx)
 {
-	SDL_Surface	*image;
 	SDL_Rect	rcDest;
 
-	image = IMG_Load("img/poo.gif");
-	rcDest.x = 0;
-	rcDest.y = 0;
-	rcDest.w = 50;
-	rcDest.h = 50;
-	SDL_BlitSurface(image, NULL, e->screen, &rcDest);
+	rcDest.x = gfx->camera[0];
+	rcDest.y = gfx->camera[1];
+	rcDest.w = MAP_W;
+	rcDest.h = MAP_H;
+	SDL_RenderCopy(gfx->e.screen, gfx->e.view, NULL, &rcDest);
 }
 
-int	event_listener(SDL_Event *ev, int *quit, t_sdl *e)
+void	move_camera(t_env *gfx)
+{
+	if (gfx->scroll[0] && gfx->camera[1] + gfx->s_speed < 0)
+		gfx->camera[1] += gfx->s_speed;
+	if (gfx->scroll[1] && gfx->camera[1] > - (MAP_H - HEIGHT - gfx->s_speed))
+		gfx->camera[1] -= gfx->s_speed;
+	if (gfx->scroll[2] && gfx->camera[0] + gfx->s_speed < 0)
+		gfx->camera[0] += gfx->s_speed;
+	if (gfx->scroll[3] && gfx->camera[0] > - (MAP_W - WIDTH - gfx->s_speed))
+		gfx->camera[0] -= gfx->s_speed;
+
+}
+
+void	mouse_event(t_env *gfx)
+{
+	int	x;
+	int	y;
+
+	SDL_GetMouseState(&x, &y);
+	if (y < 150)
+	{
+		gfx->scroll[0] = 1;
+		gfx->s_speed = (150 - y) * 5 / 150;
+	}
+	else
+		gfx->scroll[0] = 0;
+	if (y > (HEIGHT - 150))
+	{
+		gfx->scroll[1] = 1;
+		gfx->s_speed = (150 - (HEIGHT - y)) * 5 / 150;
+	}
+	else
+		gfx->scroll[1] = 0;
+	if (x < 150)
+	{
+		gfx->scroll[2] = 1;
+		gfx->s_speed = (150 - x) * 5 / 150;
+	}
+	else
+		gfx->scroll[2] = 0;
+	if (x > (WIDTH - 150))
+	{
+		gfx->scroll[3] = 1;
+		gfx->s_speed = (150 - (WIDTH - x)) * 5 / 150;
+	}
+	else
+		gfx->scroll[3] = 0;
+}
+
+int	event_listener(SDL_Event *ev, int *quit, t_env *gfx)
 {
 	while (SDL_PollEvent(ev) != 0)
 	{
@@ -77,41 +139,48 @@ int	event_listener(SDL_Event *ev, int *quit, t_sdl *e)
 		{
 			if (ev->key.keysym.sym == KEY_ESC)
 				*quit = 1;
-			else if (ev->key.keysym.sym == KEY_ENTER)
-				draw_crap(e);
-			else if (ev->key.keysym.sym == KEY_SPACE)
-				draw_bg(e);
+			else if (ev->key.keysym.sym == KEY_UP)
+				gfx->camera[1] -= 1;
+			else if (ev->key.keysym.sym == KEY_DOWN)
+				gfx->camera[1] += 1;
+			else if (ev->key.keysym.sym == KEY_LEFT)
+				gfx->camera[0] -= 1;
+			else if (ev->key.keysym.sym == KEY_RIGHT)
+				gfx->camera[0] += 1;
 		}
+		else if (ev->type == SDL_MOUSEMOTION)
+			mouse_event(gfx);
 	}
 	return (0);
 }
 
-int	gfx_core(t_sdl *e)
+int	gfx_core(t_env *gfx)
 {
 	SDL_Event	ev;
 	int			quit;
 
 	quit = 0;
-	draw_bg(e);
-	draw_menu(e);
 	while (!quit)
 	{
-		event_listener(&ev, &quit, e);
-		SDL_UpdateWindowSurface(e->win);
+		event_listener(&ev, &quit, gfx);
+		SDL_RenderClear(gfx->e.screen);
+		move_camera(gfx);
+		draw_board(gfx);
+		SDL_RenderPresent(gfx->e.screen);
 	}
 	return (0);
 }
 
 int	main(int argc, char **argv)
 {
-	t_sdl	e;
+	t_env	gfx;
 	
 	(void)argc;
 	(void)argv;
 
 	ft_putendl("GFX on.");
-	init_sdl(&e);
-	gfx_core(&e);
-	close_sdl(&e);
+	init_sdl(&gfx);
+	gfx_core(&gfx);
+	close_sdl(&gfx);
 	return (0);
 }
